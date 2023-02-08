2bf0d2141f94f4aad58f03c38df131ff6a72f448
SohailShah4 <115266244+SohailShah4@users.noreply.github.com>
2023-02-08 20:55:20 +0500

    Add files via upload

---
+++ register.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <title>REGISTER</title>
+</head>
+<body>
+  <div class="container">
+  <section class="vh-100" style="background-color: #eee;">
+  <div class="container h-100 bg-dark">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-lg-12 col-xl-11">
+        <div class="card text-black" style="border-radius: 25px;">
+          <div class="card-body p-md-5">
+            <div class="row justify-content-center">
+              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+
+                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4"><i><b><b><u>Sign up</u></b></b></i></p>
+                
+                
+               <form action="" method="post">
+                <label class="form-label"><i><b>Your ID</b></i></label>
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="text" placeholder="Enter Your ID" class="form-control" name="id">
+                    </div>
+                  </div>
+                  
+                <label class="form-label"><i><b>Your Name</b></i></label>
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="text" placeholder="Enter Your Name" class="form-control" name="name">
+                    </div>
+                  </div>
+
+                  
+                <label class="form-label"><i><b>Your Email</b></i></label>
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="text" placeholder="Enter Your Email" class="form-control" name="email">
+                    </div>
+                  </div>
+                  
+                <label class="form-label"><i><b>Your Password</b></i></label>
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="text" placeholder="Enter Your Password" class="form-control" name="password">
+                    </div>
+                  </div>
+                  
+                <label class="form-label"><i><b>Your Contact</b></i></label>
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="text" placeholder="Enter Your Contact" class="form-control" name="user_type">
+                    </div>
+                  </div>
+                  <div class="form-check d-flex justify-content-center mb-5">
+                    <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
+                    <label class="form-check-label" for="form2Example3">
+                      I agree all statements in <a href="#!">Terms of service</a>
+                    </label>
+                  </div>
+
+                  <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                    <button type="button" class="btn btn-primary btn-lg">Register</button>
+                  </div>
+
+                </form>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+  </div>
+</body>
+</html>
